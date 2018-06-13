27372c966f05b209c78a539e436c72431fd6c4d5
Yevgeniy <evgeniyjames@gmail.com>
2018-06-13 17:34:42 +0300

    +add choose sim card activity and layout

---
+++ time4parent/src/main/java/com/zamkovenko/time4parent/activity/ChooseSimCardActivity.java
@@ -0,0 +1,17 @@
+package com.zamkovenko.time4parent.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zamkovenko.time4parent.R;
+
+public class ChooseSimCardActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
+    }
+}
